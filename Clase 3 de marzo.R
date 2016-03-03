eac0407640f3bee4348a0d96ee4af1299e1c569e
Daniela Jacinto <djr1521@hotmail.com>
2016-03-03 16:32:25 -0600

    Clase 3 de marzo
    Usamos If, else

---
+++ Clase 3 de marzo.R
@@ -0,0 +1,13 @@
+x <- runif(1, min=0, max=100)
+    if(x<=30) {
+    y <- "Hola"
+} else if(x>30 && x<50) {
+    y <- "Qué onda"
+} else if(x>50 && x<60) {
+    y <- "Quiubo"
+} else if(x>60 && x<80) {
+    y <- "Qué pex"
+} else if (x>80 && x<100) {
+    y <- "Qué pedo wey"
+}
+y
