eb3edfdf5c4ffef6f0abf5a5c219c2e3ebaabddc
Daniela Jacinto <djr1521@hotmail.com>
2016-03-10 17:18:22 -0600

    Usando while
    Aprendimos a usar más de la función while

---
+++ Práctica 10 de Marzo.R
@@ -0,0 +1,21 @@
+z <- 5
+j <- 1
+nuevo <- vector("numeric", 1)
+
+while(z>=3 && z<=10) {
+    nuevo[j] <- z
+    
+    moneda <- rbinom(1, 1, 0.5)
+    
+    if(moneda==1){
+        z <- z + 1
+        nuevo[j] <- z
+        
+    } else {
+        z <- z - 1
+        nuevo[j] <- z
+    }
+    j <- j+1
+    length(nuevo) <- length(nuevo) + 1
+}
+nuevo
